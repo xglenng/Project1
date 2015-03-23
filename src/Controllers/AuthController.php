45bfd5a6624549e7e1ffe2fe41cab40348296e95
xglenng <xglenng@gmail.com>
2015-03-23 17:05:47 -0600

    initial commit

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -11,6 +11,9 @@
  */
 
 namespace Controllers;
+use Common\Authentication\fileLoaded;
+use Common\Authentication\inMemory;
+use Common\Authentication\mySQL;
 
 
 /**
@@ -28,6 +31,21 @@ class AuthController extends Controller
         $postData = $this->request->getPost();
 
         echo 'Authenticate the above two different ways' . var_dump($postData);
+
+        if ($postData->authSelect =='FileBased') {
+            $loggingUser = new fileLoaded($postData);
+        }
+        else if ($postData->authSelect =='InMemory'){
+            $loggingUser = new inMemory($postData) ;
+        }
+        else if ($postData->authSelect =='mySQL'){
+            $loggingUser = new mySQL($postData) ;
+        }
+        if ($loggingUser->authenticate()) {
+             echo "Thanks for logging in";       
+        } else {
+             echo "Sorry Please try again";
+        }
 
         // example code: $auth = new Authentication($postData['username'], $postData['password']);
     }
--- src/Views/LoginForm.php
+++ src/Views/LoginForm.php
@@ -19,6 +19,7 @@ class LoginForm extends View
             <form method="POST" action="/auth">
                 Username: <input type="text" name="username" size="15" /><br />
                 Password: <input type="password" name="password" size="15" /><br />
+                mySQL: <input type="mySQL" name="mySQL" size="15" /><br />
                 <p><input type="submit" value="Login" /></p>
             </form>
         </div>
